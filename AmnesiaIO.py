904f4c27fa318fe095564c8cf048b8d5124ae163
Jolyon <j@jolyongray.co.uk>
2017-03-23 00:34:24 +0000

    Go

---
+++ AmnesiaIO.py
@@ -0,0 +1,85 @@
+import sublime
+import sublime_plugin
+import json
+from urllib.request import Request, urlopen
+
+API_URL = 'https://amnesia.io/api';
+MSG_SUCCESS = 'Amnesia.io share link copied to clipboard';
+
+class Amnesia(sublime_plugin.WindowCommand):
+  def run(self, **args):
+    settings = sublime.load_settings('AmnesiaIO.sublime-settings')
+
+    def request(content):
+      headers = {'Content-Type': 'application/json'}
+      payload = str.encode(json.dumps({
+        'content': content,
+        'extension': settings.get('ttl'),
+        'ttl': settings.get('ttl')
+      }))
+      req = Request(API_URL, data = payload, headers = headers)
+
+      try:
+          response = urlopen(req)
+      except TypeError as error:
+          return sublime.status_message(str(error))
+      except URLError as error:
+          return sublime.status_message('Error connecting to "%s"' % API_URL)
+
+      responseJSON = json.loads(response.read().decode(response.info().get_param('charset') or 'utf-8'))
+
+      sublime.set_clipboard(str(responseJSON['url']))
+      sublime.status_message(MSG_SUCCESS)
+
+    def getRelevant():
+      view = self.window.active_view()
+      selections = view.sel()
+
+      emptySelections = map(lambda sel: sel.a == sel.b, selections)
+      if not all(emptySelections):
+        trimmedSelections = filter(lambda sel: sel.a != sel.b, selections)
+        return map(lambda s: view.substr(s), trimmedSelections)
+      elif len(selections) > 1:
+        return getLine()
+      else:
+        return getFile()
+
+    def getFile():
+      view = self.window.active_view()
+      return [view.substr(sublime.Region(0, view.size()))]
+
+    def getLine():
+      view = self.window.active_view()
+      return map(lambda region: view.substr(view.line(region)), view.sel())
+
+    def getSelections():
+      view = self.window.active_view()
+      selections = view.sel()
+
+      emptySelections = map(lambda sel: sel.a == sel.b, selections)
+      if all(emptySelections):
+        sublime.status_message('Selection empty')
+        return false
+
+      trimmedSelections = filter(lambda sel: sel.a != sel.b, selections)
+      return map(lambda s: view.substr(s), trimmedSelections)
+
+    def dispatch():
+      if 'share' in args:
+        share = args['share']
+      else:
+        share = 'relevant'
+
+      if share == 'relevant':
+        texts = getRelevant()
+      elif share == 'selection':
+        texts = getSelections()
+      elif share == 'file':
+        texts = getFile()
+      elif share == 'line':
+        texts = getLine()
+
+      if(texts):
+        request('\n'.join(texts))
+
+    dispatch()
---
+++ README.md
@@ -0,0 +1,60 @@
+# Amnesia-io
+
+[Amnesia.io](https://amnesia.io) integration for [Sublime Text 3](https://www.sublimetext.com/).
+
+This package allows you to share code via amnesia.io directly
+from the editor, and copies a share link to your clipboard.
+
+Amnesia.io is an ephemeral code sharing service with nice collaboration and privacy
+features. Read more about it at https://amnesia.io.
+
+## Usage
+
+### Installation
+
+* Copy into a folder in your Packages directory (under `Preferences -> Browse Packages...`)
+* Not currently in Package Control
+
+### Functionality
+
+The `amnesia` command by default will share the current selection, or if none and multiple cursors, the active lines, or if one cursor, the entire current file.
+
+You can choose what to share with the following args:
+
+* `file` - Share the content of the entire current buffer.
+* `selection` - Share the current highlighted selection.
+* `line` - Share the current line.
+
+Default key mappings are as follows:
+
+|                            | Windows          | Linux            | OSX             |
+|----------------------------|------------------|------------------|-----------------|
+| Share Selection/Lines/File | ctrl-alt-shift-a | ctrl-alt-shift-a | cmd-alt-shift-a |
+| Share File                 | ctrl-alt-shift-f | ctrl-alt-shift-f | cmd-alt-shift-f |
+| Share Selection            | ctrl-alt-shift-s | ctrl-alt-shift-s | cmd-alt-shift-s |
+| Share Line                 | ctrl-alt-shift-l | ctrl-alt-shift-l | cmd-alt-shift-l |
+
+To edit go to `Preferences -> Package Settings -> AmnesiaIO` and copy a command from Key Bindings - Default to Key Bindings - User to overwrite it.
+
+### Configuration
+
+#### Settings
+
+Available settings and their default values are shown below.
+
+```yaml
+      # How long amnesia.io will hold on to your code snippets in seconds
+      ttl: 6000,
+
+      # Syntax highlighting format which will be used if it cannot be discerned
+      # from your file name
+      defaultFormat: "bash"
+```
+## Development
+
+To edit, clone into your packages directory and edit `AmnesiaIO.py`:
+
+```bash
+  git clone https://github.com/jolyongray/amnesia-sublime
+  cd amnesia-atom
+```
